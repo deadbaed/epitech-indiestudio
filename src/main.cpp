a9de552dbb3aa6d7803e48299fb57e59d9e3b050
Pierre <barillec.pierre@gmail.com>
2020-06-02 19:59:25 +0200

    ADD - begin, open window

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,44 @@
 ** main
 */
 
-#include <iostream>
+#include "irrlicht.h"
+
+using namespace irr;
+
+using namespace core;
+using namespace scene;
+using namespace video;
+using namespace io;
+using namespace gui;
+
 
 int main(void)
 {
-    std::cout << "hello world" << std::endl;
-    return (0);
+    IrrlichtDevice *device =
+            createDevice( video::EDT_SOFTWARE, dimension2d<u32>(640, 480), 16,
+                          false, false, false, 0);
+
+    if (!device)
+        return 1;
+
+    device->setWindowCaption(L"Hello World! - Irrlicht Engine Demo");
+
+    IVideoDriver* driver = device->getVideoDriver();
+    ISceneManager* smgr = device->getSceneManager();
+    IGUIEnvironment* guienv = device->getGUIEnvironment();
+
+    guienv->addStaticText(L"Hello World! This is the Irrlicht Software renderer!",
+                          rect<s32>(10,10,260,22), true);
+
+    while(device->run())
+    {
+        driver->beginScene(true, true, SColor(255,100,101,140));
+
+        smgr->drawAll();
+        guienv->drawAll();
+
+        driver->endScene();
+    }
+    device->drop();
+    return 0;
 }

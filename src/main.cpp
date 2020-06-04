6fff25270196f71738f53f4f9e9a64ae43b20420
Pierre <barillec.pierre@gmail.com>
2020-06-04 11:12:23 +0200

    FIX - clean branch master

--- src/main.cpp
+++ src/main.cpp
@@ -5,24 +5,7 @@
 ** main
 */
 
-#include <iostream>
-#include <SFML/Audio.hpp>
-
 int main(void)
 {
-    // Load a music to play
-    sf::Music music;
-    std::cout << "loading music file" << std::endl;
-    std::string filename = "../src/abc.ogg";
-    if (!music.openFromFile(filename))
-        return EXIT_FAILURE;
-
-    // Play the music
-    std::cout << "starting to play music" << std::endl;
-    music.play();
-
-    while (music.getStatus())
-        std::cout << "looping" << std::endl;
-
     return 0;
 }

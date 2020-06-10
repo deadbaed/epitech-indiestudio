c0fde4a1659877b740aed07926c1e57644c70374
Philippe Loctaux <p@philippeloctaux.com>
2020-06-11 00:18:12 +0200

    ADD - Storage: constructBasePath: Added Win32 implementation

--- Storage/Storage.cpp
+++ Storage/Storage.cpp
@@ -7,8 +7,18 @@
 
 #include "Storage.hpp"
 
+#include <iostream>
+
+#if defined(STORAGE_RELEASE) && defined(_WIN32)
+#include <windows.h>
+#include <shlobj.h>
+#endif
+
 Storage::Storage() {
     this->BasePath = constructBasePath();
+
+    if (this->BasePath.empty() == true)
+        abort();
 }
 
 Storage::~Storage() {
@@ -20,13 +30,35 @@ fs::path Storage::constructBasePath() {
     fs::path path;
 
 #ifdef STORAGE_DEBUG
+    /* Debug implementation, use only for development */
     path = fs::current_path() /= STORAGE_FOLDER_NAME;
 #endif
 
-#ifdef STORAGE_RELEASE
-    path = fs::current_path() /= "release mode";
+#if defined(STORAGE_RELEASE) && defined(_WIN32)
+    /* Windows implementation */
+
+    PWSTR path_tmp;
+
+    /* Attempt to get user's AppData folder */
+    auto get_folder_path_ret = SHGetKnownFolderPath(FOLDERID_RoamingAppData, 0, nullptr, &path_tmp);
+
+    /* Error check */
+    if (get_folder_path_ret != S_OK) {
+        CoTaskMemFree(path_tmp);
+        return path;
+    }
+
+    /* Convert the Windows path type to a C++ path */
+    path = path_tmp;
+
+    /* Free memory :) */
+    CoTaskMemFree(path_tmp);
+
+    path /= STORAGE_FOLDER_NAME;
+#endif
+
+#if defined(STORAGE_RELEASE) && defined(__linux__)
     // Linux: get env variable $XDG_CONFIG_HOME /= STORAGE_FOLDER_NAME;
-    // Windows: get env variable APPDATA /= STORAGE_FOLDER_NAME;
 #endif
 
     return path;

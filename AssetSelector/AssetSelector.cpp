45cd6aae794c6d578c8f9bc8b94464f75d040eb2
Philippe Loctaux <p@philippeloctaux.com>
2020-06-14 13:52:15 +0200

    MAJ - AssetSelector: Linux: Works in Release and Debug

--- AssetSelector/AssetSelector.cpp
+++ AssetSelector/AssetSelector.cpp
@@ -5,7 +5,6 @@
 ** Created by phil on 6/14/20
 */
 
-#include <iostream>
 #include "AssetSelector.hpp"
 
 fs::path AssetSelector(const std::string &asset) {
@@ -14,26 +13,22 @@ fs::path AssetSelector(const std::string &asset) {
 #if defined(ASSET_SELECTOR_RELEASE) && defined(__linux__)
     if (fs::current_path() == std::getenv("HOME"))
         AssetPath = "/usr/share/IndieStudio/assets";
-    else
-        std::cerr << "not in installed mode (with package)" << std::endl;
 #endif
 
 #if defined(ASSET_SELECTOR_RELEASE) && defined(_WIN32)
-    std::cout << "win32 implementation will go here" << std::endl;
+    /* win32 implementation will go here */
 #endif
 
     if (AssetPath.empty()) {
-        AssetPath = fs::current_path();
-        std::cout << "current path: " << AssetPath << std::endl;
-        fs::path tmp = AssetPath;
-        tmp /= "assets";
-        if (!fs::exists(tmp)) {
-            std::cout << tmp << "does not exist" << std::endl;
-            AssetPath = AssetPath.parent_path();
+        /* look for the assets folder in current directory */
+        AssetPath = fs::current_path() /= "assets";
+
+        if (!fs::exists(AssetPath)) {
+            /* try the parent directory */
+            AssetPath = fs::current_path().parent_path() /= "assets";
         }
     }
 
-    std::cout << "current path: " << AssetPath << std::endl;
     AssetPath /= asset;
 
     return AssetPath;

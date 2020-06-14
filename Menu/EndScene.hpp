1c0636d4b2937bee6e95daa31c9f944b0531c962
Pierre <barillec.pierre@gmail.com>
2020-06-14 03:09:28 +0200

    UPDATE - define of bgend

--- Menu/EndScene.hpp
+++ Menu/EndScene.hpp
@@ -9,7 +9,7 @@
 #include "GameScene.hpp"
 #include "Sound.hpp"
 
-#define BACKGROUND_END "../assets/end/bgend.png"
+#define BACKGROUND_END "../assets/end/bgend.jpg"
 #define MUSIC_END "../assets/audio/music_game.ogg"
 #define RESTART "../assets/end/restart.png"
 #define RESTART_DARK "../assets/end/restart_fonce.png"

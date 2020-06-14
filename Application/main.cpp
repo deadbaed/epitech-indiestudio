aca630ab529e650e3592861fa3eab21406c534d1
Philippe Loctaux <p@philippeloctaux.com>
2020-06-14 12:36:39 +0200

    MAJ - removed asset selector usage in main

--- Application/main.cpp
+++ Application/main.cpp
@@ -18,7 +18,6 @@
 #include "SelectSkin.hpp"
 #include "SelectSkins.hpp"
 #include "EndScene.hpp"
-#include "AssetSelector.hpp"
 
 /* Win32: Remove the console application */
 //#ifdef _IRR_WINDOWS_
@@ -27,9 +26,6 @@
 //#endif
 
 int main(void) {
-
-    std::cout << AssetSelector("test/folder/file.png") << std::endl;
-
     std::shared_ptr<IrrlichtController> c = std::make_shared<IrrlichtController>();
     c->Init(irr::video::E_DRIVER_TYPE::EDT_OPENGL, WIDTH, HEIGHT);
     c->_device->setWindowCaption(L"Indie Studio");
